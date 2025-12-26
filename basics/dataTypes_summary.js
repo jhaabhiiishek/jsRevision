4fa404cd5ef060ec8c793c003cfbf4eab04d8099
Abhishek Jha <abhi10092003@gmail.com>
2025-12-26 13:01:24 +0530

    More about Internal Memory - Stacks and Heaps

--- basics/dataTypes_summary.js
+++ basics/dataTypes_summary.js
@@ -19,14 +19,14 @@ let userEmail = undefined;//undefined
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id==anotherId);//true
-console.log(id===anotherId);//false
+// console.log(id==anotherId);//true
+// console.log(id===anotherId);//false
 
 const bigNumber= 94872838809487930847n //makes it bigint adding n at the end
 
 // TypeScript is statically typed
 
-// Non-Primitive - Reference 
+// Non-Primitive - Reference type
 /*
 	3 main primarily:
 	Array
@@ -43,9 +43,44 @@ let person = {
 const myfunc = function myfunc(){
 	console.log("Hello World")
 }
-myfunc // prints nothing
-myfunc() //prints Hello World
+// myfunc // prints nothing
+// myfunc() //prints Hello World
 
-console.log(typeof bigNumber) //bigint
-console.log(typeof myfunc) //function - object
-console.log(typeof heroes) //object
+// console.log(typeof bigNumber) //bigint
+// console.log(typeof myfunc) //function - object
+// console.log(typeof heroes) //object
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Memory 
+
+// 2 types: Stack and Heap
+
+// Stack(Primitive) - Gives copy of original value to change
+// Heap(Non-Primitive) - Gives reference of original value to change
+
+let myYoutubeName ="AbhiiishekKumarJha"
+
+let anotherName = myYoutubeName
+anotherName = "abhishekJha"
+
+console.log(myYoutubeName)
+console.log(anotherName)
+// As it is primitive type both will be different  (Stack, pass by copy)
+
+let userOne = {
+	id:1,
+	name:'Abhishek',
+	email:'abhi@gmail.com'
+}
+let userTwo = userOne
+
+userTwo.email="abhiii@gmail.com"
+
+console.log(userOne)
+console.log(userTwo)
+
+// As it is non primitive it is stored in Heap,
+// - thus referenced
+// - thus only a reference is passed 
