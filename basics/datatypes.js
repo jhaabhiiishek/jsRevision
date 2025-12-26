6d93cfe6d7fd2703f36646823cb750bb62cdc889
Abhishek Jha <abhi10092003@gmail.com>
2025-12-26 10:38:56 +0530

    Basic datatypes learnt

---
+++ basics/datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //Treats all js as new version
+
+// alert("hello") can't be used without browser
+
+console.log(3
+	+3);console.log("Abhishek")
+//Not wrong but there's no readability
+
+// Ecma -> decides standards, link: tc39.es/ecma262
+
+let name = "Abhishek"//string
+let age = 22 //number
+let isLoggedIn = false //boolean
+
+// numer=> 2^32
+// bigint
+// string=> ""
+// boolean=> true/false
+// null=> standalone value and datatype 
+// - empty value representation 
+// - type object
+// undefined=> variable declared but not defined
+// - type undefined
+// symbol=> unique
+let null1 = null
+let null2 = null
+// console.log(null1==null2);// true
+
+// console.log(typeof null1) //=>object
+console.log(typeof undefined) //=>undefined
--- basics/test.js
+++ basics/test.js
@@ -2,6 +2,18 @@ const accountId = 0;
 let accountEmail = "abhi10092003@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
+// Possible to declare this way
+let accountState;
 
-accountId = 2
+// accountId = 2 (not allowed)
 console.log(accountId);
+
+/*
+	Prefer not to use var because of block scope issues
+*/
+
+accountEmail ="aj@ab.com"
+accountPassword="2794809"
+accountCity = "Bengaluru"
+console.log([accountState,accountEmail,accountId,accountPassword,accountCity])
+//accountState = undefined as the value was never initialized
