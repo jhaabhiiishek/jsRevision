92c440c444f82029a6259e9cf97e9bfd12216019
Abhishek Jha <abhi10092003@gmail.com>
2025-12-27 08:57:58 +0530

    Functions2

--- 03_basics/01functions.js
+++ 03_basics/01functions.js
@@ -31,4 +31,36 @@ function loginUserMessage(username="sam"){ //sam is default value and in this ca
 	return `${username} just logged in`
 }
 // console.log(loginUserMessage("Abhishek"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+function calculateCartPrice(username,...num1){ //-> Rest operator
+	let cartValue = 0;
+	for(let i=0;i<num1.length;i++){
+		cartValue+=num1[i]
+	}
+	return `${username} your cart value is: ${cartValue}`
+}
+
+// console.log(calculateCartPrice("Abhishek",200,400,500))
+const user ={
+	username:"Abhishek",
+	price:2500
+}
+
+function handleObject(anyObject){
+	console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+
+console.log(handleObject(user))
+console.log(handleObject({
+	username:"Abhi",
+	price:2000
+}))
+
+const newArray = [200,400,100,600]
+function returnSecondValue(getArray){
+	return getArray[1]
+}
+console.log(returnSecondValue(newArray));
+console.log(returnSecondValue([200,400,100,600]));
