04ef7f3c2009499966b23db8786a46dae221d2d5
Abhishek Jha <abhi10092003@gmail.com>
2025-12-27 20:25:13 +0530

    iterations - for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,41 @@
+
+for(let index=0;index<10;index++){
+	const element = index
+	if(element==5){
+		// console.log("5 is best");
+	}
+	// console.log(element)
+}
+
+// console.log(element);
+
+// for (let i = 0; i < 10; i++) {
+// 	console.log(`Outer Loop`);
+// 	for (let j = 0; j < i; j++) {
+// 		console.log(`Inner Loop`);
+// 	}
+// }
+
+// let myArray = ["flash","Batman","Superman"]
+// console.log(myArray.length);
+
+// for (let index = 0; index < myArray.length; index++) {
+// 	const element = myArray[index];
+// 	console.log(element);
+// }
+
+// break and continue
+// for (let index = 1; index <= 20; index++) {
+// 	if(index==5){
+// 		console.log(`Detected 5`);
+// 		break
+// 	}
+// 	console.log(`Value of i is ${index}`);
+// }
+for (let index = 1; index <= 20; index++) {
+	if(index==5){
+		console.log(`Detected 5`);
+		continue
+	}
+	console.log(`Value of i is ${index}`);
+}
---
+++ test/questionYouFailed.js
@@ -0,0 +1,36 @@
+// // const obj1 = {
+// // 	first:{
+// // 		a:"Good",
+// // 		b:"Bad",
+// // 	},
+// // 	second:{
+// // 		third:{
+// // 			c:"Great"
+// // 		},
+// // 		fourth:{
+// // 			d:"Bad",
+// // 			e:"Terrible"
+// // 		}
+// // 	}
+// // }
+
+// // const obj2 = obj1.flat(Infinity)
+// // console.log(obj2);
+
+// const val = {
+// 	a:{
+// 		b:"iabid",
+// 		d:"fiebc"
+// 	},
+// 	c:"efbu"
+// }
+
+// // answer:
+// // val.a.b="iabid"
+// // val.a.d="fiebc"
+// // val.c="efbu"
+
+// for(){
+
+// }
+// console.log(newVal);
