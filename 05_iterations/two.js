fc44bf808a6359e86002a9fc062bec9af180404d
Abhishek Jha <abhi10092003@gmail.com>
2025-12-27 20:42:28 +0530

    Iterations - do while and while

---
+++ 05_iterations/two.js
@@ -0,0 +1,17 @@
+let index = 0
+// while(index<=10){
+// 	console.log(`Value of index is ${index}`);	
+// 	index = index+2;
+// }
+// let myArr = ['flash','batman','superman']
+// arr = 0
+// while(myArr.length){
+// 	console.log(`Value is ${myArr[myArr.length-1]}`);
+// 	myArr.length--
+// }
+//Very important! this works!
+
+let score = 11
+do{
+	console.log(`${score} is your score`);
+}while(score++<10)
